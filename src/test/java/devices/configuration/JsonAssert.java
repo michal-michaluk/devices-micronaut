7ffe26e980d5cbe300aa4e1f2b94632653296f69
Michal Michaluk <michal.michaluk@bottega.com.pl>
2024-06-27 11:41:52 +0200

    solution of exercise 02

---
+++ src/test/java/devices/configuration/JsonAssert.java
@@ -0,0 +1,75 @@
+package devices.configuration;
+
+import devices.configuration.tools.JsonConfiguration;
+import lombok.SneakyThrows;
+import org.intellij.lang.annotations.Language;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+public class JsonAssert {
+
+    private final String actual;
+
+    public JsonAssert(String actual) {
+        this.actual = actual;
+    }
+
+    public static JsonAssert assertThat(Object object) {
+        return new JsonAssert(json(object));
+    }
+
+    public static JsonAssert assertThat(@Language("JSON") String json) {
+        return new JsonAssert(json);
+    }
+
+    @SneakyThrows
+    public JsonAssert isExactlyLike(Object object) {
+        assertJson(json(object), actual, true);
+        return this;
+    }
+
+    @SneakyThrows
+    public JsonAssert isExactlyLike(@Language("JSON") String json) {
+        assertJson(json, actual, true);
+        return this;
+    }
+
+    @SneakyThrows
+    public JsonAssert isExactlyLike(@Language("JSON") String json, Object... params) {
+        assertJson(json.formatted(params), actual, true);
+        return this;
+    }
+
+    @SneakyThrows
+    public JsonAssert hasFieldsLike(Object object) {
+        assertJson(json(object), actual, false);
+        return this;
+    }
+
+    @SneakyThrows
+    public JsonAssert hasFieldsLike(@Language("JSON") String json) {
+        assertJson(json, actual, false);
+        return this;
+    }
+
+    @SneakyThrows
+    public JsonAssert hasFieldsLike(@Language("JSON") String json, Object... params) {
+        assertJson(json.formatted(params), actual, false);
+        return this;
+    }
+
+    @SneakyThrows
+    public static String json(Object object) {
+        return JsonConfiguration.json(object);
+    }
+
+    private static void assertJson(String expected, String actual, boolean strict) throws JSONException {
+        try {
+            JSONAssert.assertEquals(expected, actual, strict);
+        } catch (Throwable e) {
+            System.err.println("Actual:");
+            System.err.println(actual);
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/devices/configuration/intervals/IntervalRulesDocumentRepositoryTest.java
@@ -0,0 +1,77 @@
+package devices.configuration.intervals;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static devices.configuration.JsonAssert.assertThat;
+
+@MicronautTest
+class IntervalRulesDocumentRepositoryTest {
+
+    @Inject
+    private IntervalRulesDocumentRepository repository;
+
+    @Test
+    void shouldSaveAndLoadIntervalRules() {
+        // when
+        repository.save(IntervalRulesFixture.currentRules());
+        var result = repository.get();
+
+        // then
+        assertThat(result).hasFieldsLike(IntervalRulesFixture.currentRules());
+        assertThat(result).isExactlyLike("""
+                {
+                  "byIds": [
+                    {
+                      "seconds": 600,
+                      "devices": [
+                        "ALF-2844179",
+                        "ALF-9571445",
+                        "CS_7155_CGC100",
+                        "EVB-P4562137",
+                        "EVB-P9287312"
+                      ]
+                    },
+                    {
+                      "seconds": 2700,
+                      "devices": [
+                        "EVB-P0984003",
+                        "EVB-P15079256",
+                        "EVB-P1515526",
+                        "EVB-P1515640",
+                        "t53_8264_019"
+                      ]
+                    }
+                  ],
+                  "byModel": [
+                    {
+                      "seconds": 60,
+                      "vendor": "Alfen BV",
+                      "model": "NG920-5250[6-9]",
+                      "firmware": null
+                    },
+                    {
+                      "seconds": 10,
+                      "vendor": "ChargeStorm AB",
+                      "model": "Chargestorm Connected",
+                      "firmware": "1[.]2[.].*"
+                    },
+                    {
+                      "seconds": 60,
+                      "vendor": "ChargeStorm AB",
+                      "model": "Chargestorm Connected",
+                      "firmware": null
+                    },
+                    {
+                      "seconds": 120,
+                      "vendor": "EV-BOX",
+                      "model": "G3-M5320E-F2.*",
+                      "firmware": null
+                    }
+                  ],
+                  "defSeconds": 1800
+                }
+                """);
+    }
+}
